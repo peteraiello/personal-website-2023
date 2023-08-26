f68fc1e83b34de70040d67bb16d33026949cbcdd
peteraiello <peter.aiello89@gmail.com>
2023-08-26 18:10:32 +0100

    add storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 module.exports = {
     content: [
+        './pages/**/*.{js,jsx,ts,tsx}',    
         './index/**/*.{js,jsx,ts,tsx}',
         './components/**/*.{js,jsx,ts,tsx}',
         './modules/**/*.{js,jsx,ts,tsx}',
@@ -13,4 +14,4 @@ module.exports = {
       extend: {},
     },
     plugins: [],
-  };
+};
