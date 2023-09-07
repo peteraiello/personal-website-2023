1a2d9f4753b860749119e2e1ae0b53e912649415
peteraiello <peter.aiello89@gmail.com>
2023-09-07 18:25:34 +0100

    Use JSON as Array Data

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,16 @@ import styles from '../styles/Home.module.css';
 import {Header} from '../modules/header';
 import React from "react";
 import {Articles} from '../modules/writing/articles';
+import navItems from '../modules/header/navItems.json'
 
 export default function Home() {
+
+  console.log(navItems?.items);
 
   return (
 
     <>
-      <Header />
+      <Header items={navItems?.items} />
 
       <Articles
           title={'writing'}
