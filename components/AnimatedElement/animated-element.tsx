04d8c183792f77f12d463189d941ccae2186fa52
peteraiello <hello@peteraiello.dev>
2025-02-07 09:04:47 +0000

    Speed tweak on animated elements

--- components/AnimatedElement/animated-element.tsx
+++ components/AnimatedElement/animated-element.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import { useRef } from "react";
 import {useInView} from 'framer-motion';
 
+interface AnimatedElementProps {
+    children?
+    speed?: 'slow' | 'medium' | 'fast',
+    index?: number
+}
+
+
 export const AnimatedElement = ({
     children,
+    speed,
     index,
-}) => {
+}:AnimatedElementProps) => {
 
     const ref = useRef(null);
 
@@ -15,31 +23,87 @@ export const AnimatedElement = ({
     });  
 
     function getTheDelay(index){
-        switch(index) {
-            case(0):
-                return 'delay-0';
-            case(1): 
-                return 'delay-[200ms]'
-            case(2):
-                return 'delay-[400ms]'
-            case(3): 
-                return 'delay-[600ms]'
-            case(4): 
-                return 'delay-[800ms]'
-            case(5): 
-                return 'delay-[1000ms]'
-            case(6): 
-                return 'delay-[1200ms]'
-            case(7): 
-                return 'delay-[1400ms]'
-            case(8): 
-                return 'delay-[1400ms]'
-            case(9): 
-                return 'delay-[1600ms]'
-            case(10): 
-                return 'delay-[1800ms]'
-            default:
-                return 'delay-100'
+        if(speed == 'slow') {
+            switch(index) {
+                case(0):
+                    return 'delay-0';
+                case(1): 
+                    return 'delay-[200ms]'
+                case(2):
+                    return 'delay-[400ms]'
+                case(3): 
+                    return 'delay-[600ms]'
+                case(4): 
+                    return 'delay-[800ms]'
+                case(5): 
+                    return 'delay-[1000ms]'
+                case(6): 
+                    return 'delay-[1200ms]'
+                case(7): 
+                    return 'delay-[1400ms]'
+                case(8): 
+                    return 'delay-[1400ms]'
+                case(9): 
+                    return 'delay-[1600ms]'
+                case(10): 
+                    return 'delay-[1800ms]'
+                default:
+                    return 'delay-100'
+            }
+        } else if(speed == 'medium') {
+            switch(index) {
+                case(0):
+                    return 'delay-0';
+                case(1): 
+                    return 'delay-[150ms]'
+                case(2):
+                    return 'delay-[300ms]'
+                case(3): 
+                    return 'delay-[450ms]'
+                case(4): 
+                    return 'delay-[600ms]'
+                case(5): 
+                    return 'delay-[750ms]'
+                case(6): 
+                    return 'delay-[900ms]'
+                case(7): 
+                    return 'delay-[1050ms]'
+                case(8): 
+                    return 'delay-[1200ms]'
+                case(9): 
+                    return 'delay-[1350ms]'
+                case(10): 
+                    return 'delay-[1500ms]'
+                default:
+                    return 'delay-100'
+            }
+        } else if(speed == "fast") {
+            switch(index) {
+                case(0):
+                    return 'delay-0';
+                case(1): 
+                    return 'delay-[100ms]'
+                case(2):
+                    return 'delay-[200ms]'
+                case(3): 
+                    return 'delay-[300ms]'
+                case(4): 
+                    return 'delay-[400ms]'
+                case(5): 
+                    return 'delay-[500ms]'
+                case(6): 
+                    return 'delay-[600ms]'
+                case(7): 
+                    return 'delay-[700ms]'
+                case(8): 
+                    return 'delay-[800ms]'
+                case(9): 
+                    return 'delay-[900ms]'
+                case(10): 
+                    return 'delay-[1000ms]'
+                default:
+                    return 'delay-100'
+            }
         }
     }
 
--- components/tag/tag.tsx
+++ components/tag/tag.tsx
@@ -14,7 +14,7 @@ export const Tag = ({
     index
 }:TagProps) => {
     return(
-        <AnimatedElement index={index}>
+        <AnimatedElement index={index} speed={'fast'}>
             <div className="bg-transparent ring-1 ring-darkGray dark:ring-white rounded-[10px] py-2 px-6">
                 {children &&
                     <p className="text-sm md:text-body font-bold dark:text-white">{children}</p>
