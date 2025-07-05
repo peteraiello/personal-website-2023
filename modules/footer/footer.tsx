a186ad42c4662b82746046629f2839e63c1eb47c
peteraiello <hello@peteraiello.dev>
2025-07-05 22:07:35 +0100

    website carbon 🌳

--- modules/footer/footer.tsx
+++ modules/footer/footer.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-// import { TextContent } from "../../components/typography/textContent";
+import React, {useContext} from "react";
 import { TextContent } from "../../components/typography/textContent";
+import { WebsiteCarbonBadge } from "react-websitecarbon-badge";
+import { ThemeContext } from "../../context/ThemeContext/ThemeContext";
+
 
 
 interface FooterProps {
@@ -13,15 +15,21 @@ interface FooterProps {
 export const Footer = ({
     footerText
 }:FooterProps) => {
+
+    const {darkThemeActive} = useContext(ThemeContext);
+
     return(
         <footer>
-            <div className="wrapper max-w-[1200px] px-5 grid grid-cols-12">
-                <div className="py-md lg:py-[56px] col-span-12 lg:col-start-4 lg:col-span-8">
+            <div className="wrapper max-w-[1200px] px-5 grid grid-cols-12 gap-4  py-[56px]">
+                <div className="col-span-12 md:col-span-8">
                     {footerText &&
                     <div className="markdown-text footer-text">
                         <TextContent isProject={true}>{footerText}</TextContent>
                     </div>
                     }
+                </div>
+                <div className="col-span-12 md:col-span-4">
+                    <WebsiteCarbonBadge dark={darkThemeActive} url="peteraiello.dev" co2="0.07" percentage="94" />
                 </div>
             </div>
         </footer>
--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -41,6 +41,5 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 export function getAllProjects(fields: string[] = []) {
   const slugs = getPostSlugs()
   const posts = slugs.map((slug) => getPostBySlug(slug, fields)).sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
-  console.log("posts", posts);
   return posts;
 }
